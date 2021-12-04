fd47d59e1417f485419b91485ce787a0bc63ee56
Simeskaa <simen.hansen.skaara@gmail.com>
2021-12-04 20:02:21 +0100

    Update airDynamics.ino

--- airDynamics/airDynamics.ino
+++ airDynamics/airDynamics.ino
@@ -1,16 +1,78 @@
-const int INSERT_NAME_HERE = "Navnet ditt her";
+/******************************************
+ *
+ * This example works for both Industrial and STEM users.
+ *
+ * Developed by Jose Garcia, https://github.com/jotathebest/
+ *
+ * ****************************************/
 
+/****************************************
+ * Include Libraries
+ ****************************************/
+#include "UbidotsEsp32Mqtt.h"
 
-void setup() {
-  Serial.begin(9600);
-}
+/****************************************
+ * Define Constants
+ ****************************************/
+const char *UBIDOTS_TOKEN = "BBFF-lxUrvf1VPtA5yDPTGKoTrtGif4tfHJ";  // Put here your Ubidots TOKEN
+const char *WIFI_SSID = "CanYouGuessIt";      // Put here your Wi-Fi SSID
+const char *WIFI_PASS = "H0wD1dY0uGuess1t???";      // Put here your Wi-Fi password
+const char *DEVICE_LABEL = "wind";   // Put here your Device label to which data  will be published
+const char *VARIABLE_LABEL = "test"; // Put here your Variable label to which data  will be published
 
-void loop() {
-  congratz(INSERT_NAME_HERE);
-}
+const int PUBLISH_FREQUENCY = 1000; // Update rate in milliseconds
 
-int congratz(INSERT_NAME_HERE)
+unsigned long timer;
+uint8_t analogPin = 34; // Pin used to read data from GPIO34 ADC_CH6.
+
+Ubidots ubidots(UBIDOTS_TOKEN);
+
+/****************************************
+ * Auxiliar Functions
+ ****************************************/
+
+void callback(char *topic, byte *payload, unsigned int length)
+{
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++)
   {
-  Serial.print("Welcome to the internet! ");
-  Serial.println(INSERT_NAME_HERE); 
+    Serial.print((char)payload[i]);
   }
+  Serial.println();
+}
+
+/****************************************
+ * Main Functions
+ ****************************************/
+
+void setup()
+{
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  // ubidots.setDebug(true);  // uncomment this to make debug messages available
+  ubidots.connectToWifi(WIFI_SSID, WIFI_PASS);
+  ubidots.setCallback(callback);
+  ubidots.setup();
+  ubidots.reconnect();
+
+  timer = millis();
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+  if (!ubidots.connected())
+  {
+    ubidots.reconnect();
+  }
+  if (abs(millis() - timer) > PUBLISH_FREQUENCY) // triggers the routine every 5 seconds
+  {
+    float value = analogRead(analogPin);
+    ubidots.add(VARIABLE_LABEL, value); // Insert your variable Labels and the value to be sent
+    ubidots.publish(DEVICE_LABEL);
+    timer = millis();
+  }
+  ubidots.loop();
+}
