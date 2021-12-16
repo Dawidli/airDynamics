cf749997894995814ec491dc41545598b5f1baf1
Simeskaa <81254666+Simeskaa@users.noreply.github.com>
2021-12-16 18:46:38 +0100

    Multisensor working ;)

--- airDynamics/airDynamics.ino
+++ airDynamics/airDynamics.ino
@@ -1,34 +1,36 @@
-  /******************************************
- *
- * This example works for both Industrial and STEM users.
- 
- * Developed by Jose Garcia, https://github.com/jotathebest/
- *
- * ****************************************/
+/******************************************
+
+  This example works for both Industrial and STEM users.
+
+  Developed by Jose Garcia, https://github.com/jotathebest/
+
+* ****************************************/
 
 /****************************************
- * Include Libraries
+   Include Libraries
  ****************************************/
 #include "UbidotsEsp32Mqtt.h"
 
 /****************************************
- * Define Constants
+   Define Constants
  ****************************************/
 const char *UBIDOTS_TOKEN = "BBFF-lxUrvf1VPtA5yDPTGKoTrtGif4tfHJ";  // Put here your Ubidots TOKEN
-const char *WIFI_SSID = "Get-2G-DCFCD1"; //"CanYouGuessIt";      
-const char *WIFI_PASS = "347BECJ7T4"; //"H0wD1dY0uGuess1t???";   
+const char *WIFI_SSID = "Get-2G-DCFCD1"; //"CanYouGuessIt";
+const char *WIFI_PASS = "347BECJ7T4"; //"H0wD1dY0uGuess1t???";
 const char *DEVICE_LABEL = "wind";   // Put here your Device label to which data  will be published
-const char *VARIABLE_LABEL = "test"; // Put here your Variable label to which data  will be published
+const char *VARIABLE_LABEL_1 = "anemometer_1"; // Put here your Variable label to which data  will be published
+const char *VARIABLE_LABEL_2 = "anemometer_2"; // Put here your Variable label to which data  will be published
 
 const int PUBLISH_FREQUENCY = 1000; // Update rate in milliseconds
 
 unsigned long timer;
-uint8_t analogPin = 34; // Pin used to read data from GPIO34 ADC_CH6.
+uint8_t analogPin_1 = 34; // Pin used to read data from GPIO34 ADC_CH6.
+uint8_t analogPin_2 = 32;
 
 Ubidots ubidots(UBIDOTS_TOKEN);
 
 /****************************************
- * Auxiliar Functions
+   Auxiliar Functions
  ****************************************/
 
 void callback(char *topic, byte *payload, unsigned int length)
@@ -44,7 +46,7 @@ void callback(char *topic, byte *payload, unsigned int length)
 }
 
 /****************************************
- * Main Functions
+   Main Functions
  ****************************************/
 
 void setup()
@@ -69,29 +71,28 @@ void loop()
   }
   if (abs(millis() - timer) > PUBLISH_FREQUENCY) // triggers the routine every 5 seconds
   {
+  ubidots.add(VARIABLE_LABEL_1, anemometer_reading( analogPin_1)); // Insert your variable Labels and the value to be sent
+  ubidots.add(VARIABLE_LABEL_2, anemometer_reading( analogPin_2));
+  ubidots.publish(DEVICE_LABEL);
+  timer = millis();
 
-    float sensorValue = analogRead(analogPin);
-    float voltage = (sensorValue*0.001221001);//esp har en maks analogread på 5 v og bits avlesning på 4095 dermed deler en på 5/4095
-    float wind_speed = mapfloat(voltage, 0.4, 2, 0, 32.4);
-    
-    Serial.println(voltage);
-    
-    
-    //float value = analogRead(analogPin);
-    
-    ubidots.add(VARIABLE_LABEL, wind_speed); // Insert your variable Labels and the value to be sent
-    ubidots.publish(DEVICE_LABEL);
-    timer = millis();
   }
   ubidots.loop();
 }
 
 float mapfloat(float x, float in_min, float in_max, float out_min, float out_max)
-    {
-    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-    }
+{
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
 
 
+float anemometer_reading(float sensor)
+{
+  float sensorValue = analogRead(sensor);
+  float voltage = (sensorValue * 0.001221001); //esp har en maks analogread på 5 v og bits avlesning på 4095 dermed deler en på 5/4095
+  float wind_speed = mapfloat(voltage, 0.38, 2, 0, 32.4);
 
+  return wind_speed;
+}
 
-    /* ME FANT UT AV DET BITCHES*/
+/* ME FANT UT AV DET BITCHES*/
